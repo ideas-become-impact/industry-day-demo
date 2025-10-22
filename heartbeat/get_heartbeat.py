c6f7d7c74d4fcd6553f60aeaeec5da69ffd44206
acdu01 <annacdu1@gmail.com>
2025-10-21 22:38:45 -0400

    changed into function

--- heartbeat/get_heartbeat.py
+++ heartbeat/get_heartbeat.py
@@ -1,18 +1,22 @@
 import serial
 import time
 
-ser = serial.Serial('COM9', 115200, timeout=1) 
+def get_heartbeat(COM_port):
+    ser = serial.Serial(COM_port, 115200, timeout=1) 
 
-time.sleep(2) 
+    time.sleep(2) 
 
-try:
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').strip() # Read a line, decode, and remove whitespace
-            print(f"{line}")
-        time.sleep(0.1) # Small delay to prevent busy-waiting
-except KeyboardInterrupt:
-    print("Program terminated by user.")
-finally:
-    ser.close() # Close the serial port when done
-    print("Serial port closed.")
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').strip() # Read a line, decode, and remove whitespace
+                hb_parts = line.split(",")
+                print(hb_parts[-1])
+                
+
+            time.sleep(0.1) # Small delay to prevent busy-waiting
+    except KeyboardInterrupt:
+        print("Program terminated by user.")
+    finally:
+        ser.close() # Close the serial port when done
+        print("Serial port closed.")
