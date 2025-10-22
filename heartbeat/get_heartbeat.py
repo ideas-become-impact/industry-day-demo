7fd6ff7c84d4c65aa016523cb6af462a63847696
acdu01 <annacdu1@gmail.com>
2025-10-22 04:33:42 -0400

    get latest heartbeat data

--- heartbeat/get_heartbeat.py
+++ heartbeat/get_heartbeat.py
@@ -45,3 +45,19 @@ def get_heartbeat(COM_port, button_clicked):
 
     return train, output
 
+def get_latest_heartbeat(COM_port):
+    "Get the latest 29 heartbeat data from serial port for testing purpose, return array of ints"
+    ser = serial.Serial(COM_port, 115200, timeout=1)
+    time.sleep(2)
+    heartbeat_data = []
+    try:
+        while len(heartbeat_data) < 29:
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').strip()
+                hb_parts = line.split(",")
+                heartbeat_data.append(int(hb_parts[-1]))
+        return heartbeat_data
+    except KeyboardInterrupt:
+        print("Program terminated by user.")
+    
+    return heartbeat_data
