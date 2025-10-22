e8ad97c025dc7b09f7937ac79b17f9959ff3bd6a
ram <ramzey.burdette2005@gmail.com>
2025-10-22 00:26:23 -0400

    lots of stylistic changes to app; have basic plotting working

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, jsonify, Response
+from flask import Flask, render_template, jsonify, Response, request
 from flaskwebgui import FlaskUI
-
+import random
 
 app: Flask = Flask(__name__, template_folder="template")
 
 ui: FlaskUI = FlaskUI(app=app, server="flask", width=500, height=500)
+
+push_data = True
 
 
 @app.route("/")
@@ -20,7 +22,44 @@ def data_route() -> Response:
     """
     Basic data route.
     """
-    return jsonify([1, 2, 3, 4, 5])
+    global push_data
+    data = [
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+    ]
+    return jsonify(data) if push_data else jsonify([])
+
+
+@app.route("/jumping-data-route")
+def jumping_data_route() -> Response:
+    """
+    a"""
+    global push_data
+    data = [
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+        {"x": random.randint(0, 100), "y": random.randint(0, 100)},
+    ]
+    return jsonify(data) if push_data else jsonify([])
+
+
+@app.route("/recv-output", methods=["POST"])
+def recv_output() -> Response:
+    """
+    a"""
+    global push_data
+    if request.method == "POST":
+        print(request.form)
+        data = request.get_json()
+        print(data)
+        push_data = data["push_data"]
+
+    return jsonify([])
 
 
 @app.route("/home")
@@ -35,3 +74,4 @@ def home() -> str:
 if __name__ == "__main__":
     app.run(debug=True)
     # ui.run()
+    # hi
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,42 +4,96 @@ const graph_2 = document.getElementById("graph_2")
 const button_1 = document.getElementById("button_1")
 const button_2 = document.getElementById("button_2")
 
-const labels = [' hello ', 'a', 'b', 'c', 'd']
-const data = fetch("/data_route")
-    .then(data => data.json())
-    .then(data => {
-const plottable_data = {
-    labels: labels,
-    datasets: [{
-        label: "waow",
-        backgroundColor: "#dfad8a",
-        borderColor: "rgb(255, 0, 0)",
-        data: data
-    }]
+const data_colors = {
+    backgroundColor: "#dfad8a",
+    borderColor: "rgb(255, 0, 0)",
+    borderWidth: 5,
+    radius: 0,
 }
 
-const config = {
-    type: 'line',
-    data: data,
-    options: { maintainAspectRatio: false }
-};
+let xhr = new XMLHttpRequest();
 
-const myChart = new Chart(
-    document.getElementById('graph_1_image'),
-    plottable_data,
-);
+const options = {
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        type: 'linear'
+      },
+      y: {
+        type: 'linear'
+      }
+    }
+}
 
-const myChart_2 = new Chart(
-    document.getElementById('graph_2_image'),
-    plottable_data,
-);
-    })
-    .catch(err => console.error(err))
+const charts = {}
+const data = fetch("/data_route").then(data => data.json()).then(data => {
+    const plottable_data = {
+        datasets: [{
+            ...data_colors,
+            data: data
+        }]
+    }
+
+    const config = {
+        type: 'line',
+        data: plottable_data,
+        options: options
+    };
+
+    const myChart = new Chart(
+        document.getElementById('graph_1_image'),
+        config,
+    );
+
+    charts.one = myChart;
+});
 
 
+const data2 = fetch("/jumping-data-route").then(data => data.json()).then(data => {
+    const plottable_data = {
+        datasets: [{
+            ...data_colors,
+            data: data
+        }]
+    }
+
+    const config = {
+        type: 'line',
+        data: plottable_data,
+        options: options
+    };
+
+    const myChart_2 = new Chart(
+        document.getElementById('graph_2_image'),
+        config
+    );
+
+    charts.two = myChart_2;
+
+});
+
+function update_chart(chart_number) {
+    switch (chart_number) {
+        case 1: fetch("/data_route").then(data => data.json()).then(new_data => {
+            charts.one.data.datasets[0].data = new_data;
+            charts.one.update();
+        });
+        case 2: fetch("/jumping-data-route").then(data => data.json()).then(new_data => {
+            charts.two.data.datasets[0].data = new_data;
+            charts.two.update(); 
+        });
+    }
+}
 
 function turnOnGraph(caller) {
     id = caller.id;
+
+    on_req = {"push_data": true}
+    fetch("/recv-output", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(on_req)
+    });
     if (id == "button_1") {
         caller.parentNode.style.flexGrow = 2;
         caller.parentNode.style.flexShrink = 0;
@@ -64,14 +118,41 @@ function turnOnGraph(caller) {
     if (graph_1.style.display == "flex") {
         button_1.style.display = "none";
         button_2.style.display = "flex";
+        update_chart(1);
     }
 
     if (graph_2.style.display == "flex") {
         button_2.style.display = "none";
         button_1.style.display = "flex";
+        update_chart(2);
     }
 }
 
+function turnOffGraph(caller) {
+    all_children = caller.parentNode.children;
 
+    off_req = {"push_data": false}
+    fetch("/recv-output", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(off_req)
+    });
+    for (const child of all_children) {
+        if (child.classList.contains("collection")) {
+            child.style.display = "flex";
+        }
 
-
+        if (child.classList.contains("graph")) {
+            child.style.display = "none";
+            console.log("happened! x3");
+            if (child.id == "graph_1") {
+                console.log("did hapepn!");
+                charts.one.config.data.datasets[0].data = [0, 0, 0, 0, 0];
+                charts.one.update();
+            } else if (child.id == "graph_2") {
+                charts.two.config.data.datasets[0].data = [0, 0, 0, 0, 0];
+                charts.two.update();
+            }
+        }
+    }
+}
